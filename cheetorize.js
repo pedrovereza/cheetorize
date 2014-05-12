e829098fcad071a9d1130c2022c2a855ef31d769
Andre Ramaciotti da Silva <andre@ramaciotti.com>
2014-05-12 19:18:22 -0300

    Add fake typo to words with odd length

--- cheetorize.js
+++ cheetorize.js
@@ -2,6 +2,10 @@ var cheetorize = (function () {
 
     var laughSymbols = [ "a", "e", "i", "o", "u", "h", "s"];
 
+    function setCharAt(str, index, chr) {
+        return str.substr(0,index) + chr + str.substr(index+1);
+    }
+
     function cheetorize(original) {
 		return addFakeTypoTo(original);
     }
@@ -25,6 +29,13 @@ var cheetorize = (function () {
 	function addFakeTypoTo(word) {
 		if (word.length < 3)
 			return word;
+
+        if (word.length % 2 != 0) {
+            var mid = (word.length-1) / 2;
+            var tmp = word[mid+1];
+            word = setCharAt(word, mid+1, word[mid-1]);
+            word = setCharAt(word, mid-1, tmp);
+        }
 
 		return word;
 	}
